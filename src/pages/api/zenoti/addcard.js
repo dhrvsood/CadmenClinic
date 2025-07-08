9841d18942a2e62eba1e44f2d919676199e62120
Dhruv Sood <dhruvsood27@gmail.com>
2025-07-08 16:51:04 -0400

    Revert back to addcard.js from imagelab repo

--- src/pages/api/zenoti/addcard.js
+++ src/pages/api/zenoti/addcard.js
@@ -1,42 +1,43 @@
-import axios from 'axios';
-
-const protocol = req.headers['x-forwarded-proto'] || 'https';
-const host = req.headers.host || 'localhost:3000';
-const baseURL = `${protocol}://${host}`;
+import axios from 'axios'
 
 const addCard = async (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method Not Allowed' })
+  }
+  
+  const params = JSON.parse(req.body)
+
+  const { guestId } = params
+
+  if (!guestId) {
+    return res.status(400).json({ error: 'Guest ID is required' })
+  }
+
   try {
-    const params = JSON.parse(req.body);
-    const { guestId } = params;
-
-    if (!guestId) {
-      return res.status(400).json({ error: 'Guest ID is required' });
-    }
-
     const data = {
       center_id: process.env.ZENOTI_CENTER_ID,
-      redirect_uri: `${'https://cadmen-clinic-m8tfbifdv-dhruv-soods-projects-cc84876a.vercel.app'}/paymentsuccess`,
-    };
+      redirect_uri: `${'https://cadmen-clinic-m8tfbifdv-dhruv-soods-projects-cc84876a.vercel.app'}/paymentsuccess`
+    }
 
     const config = {
       method: 'post',
       url: `https://api.zenoti.com/v1/guests/${guestId}/accounts`,
       headers: {
-        Authorization: `apikey ${process.env.ZENOTI_AUTH}`,
-        'Content-Type': 'application/json',
+        Authorization: `${process.env.ZENOTI_AUTH}`,
+        'Content-Type': 'application/json'
       },
-      data,
-    };
+      data: data
+    }
 
-    const response = await axios(config);
-    return res.status(200).json(response.data);
+    const response = await axios(config)
+    return res.status(200).json(response.data)
   } catch (err) {
-    console.error(err);
-    return res.status(500).json({ error: 'Failed to add card' });
+    console.error(err)
+    return res.status(500).json({ error: 'Failed to add card' })
   }
-};
+}
 
-export default addCard;
+export default addCard
 
   // const host = req.headers.host || '';
   // const protocol = req.headers['x-forwarded-proto'] || 'https';
