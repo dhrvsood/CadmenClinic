52f731a3d5b72d66171ce485f2deb0f471afa14e
Dhruv Sood <dhruvsood27@gmail.com>
2025-07-15 01:41:24 -0400

    WIP: Dermal Fillers filling in content

--- src/doc/services/dermal-fillers.js
+++ src/doc/services/dermal-fillers.js
@@ -1,108 +1,111 @@
 const dermal_fillers = {
     category: "skin-treatments",
     general: {
-        strikePrice: "$14.00",
-        salePrice: "$9.99",
-        perUnit: " / unit",
-        discount: "30%",
-        shortTitle: "Botox",
-        longTitle: "Botox®"
+        strikePrice: "$799",
+        salePrice: "$599",
+        perUnit: " / syringe",
+        discount: "25%",
+        shortTitle: "Dermal Filler",
+        longTitle: "Dermal Filler"
     },
     hero: {
-        img: "/media/services/botox-and-wrinkle-relaxers.jpg",
-        tagline: "Soften Fine Lines & Wrinkles",
+        img: "/media/services/dermal-filters.webp",
+        tagline: "Enhance Your Confidence",
         description: "reduce the signs of aging with quick, minimally invasive Botox treatments."
     },
     beforeAfters: [
         {
             id: 1,
-            title: "Julia's Results",
-            subtitle: "Results after one month with Botox",
-            img: "/media/testimonials/botox_Julia.avif"
+            title: "Kyra's Results",
+            subtitle: "After one month with revanesse versa",
+            img: "/media/testimonials/dermalKyra.avif"
         },
         {
             id: 2,
-            title: "Daria's Results",
-            subtitle: "Results after three weeks with Botox",
-            img: "/media/testimonials/botox_Daria.avif"
+            title: "Joceyln's Results",
+            subtitle: "After three weeks with cheek, lip, and chin fillers",
+            img: "/media/testimonials/dermalJocelyn.avif"
         },
         {
             id: 3,
-            title: "Anna’s Results",
-            subtitle: "Results after 3 PRP Sessions",
-            img: "/media/testimonials/botox_Anna.avif"
+            title: "Amy’s Results",
+            subtitle: "After two weeks with cheek, lip, and chin fillers",
+            img: "/media/testimonials/dermalAmy.avif"
         }
     ],
     whatAndHowItWorks: {
-        titlePrefix: "What is Botox",
+        titlePrefix: "What is Dermal Filler",
         titleEmphasis: "and How Does It Work?",
         points: [
             {
-                heading: "Botox is FDA-approved and trusted worldwide",
-                description: "This remarkable treatment works by temporarily relaxing targeted muscles, helping to reduce the appearance of fine lines and wrinkles caused by repeated facial expressions."
+                heading: "Dermal Fillers are FDA-approved and trusted worldwide",
+                description: "From softening deep nasolabial folds to augmenting lip volume or enhancing cheek contours, dermal fillers offer a safe, versatile solution for anyone seeking a refreshed yet natural-looking transformation."
             },
             {
-                heading: "Cater to Many Use Cases",
-                description: "Whether you’re looking to soften crow’s feet, forehead lines, or frown lines, Botox offers a safe and effective solution for achieving a rejuvenated appearance.",
-
+                heading: "Targeted Injections",
+                description: "A filler is precisely injected beneath the skin to enhance or restore volume in specific areas.",
             },
             {
-                heading: "How it Works",
-                description: "Botox blocks nerve signals to specific muscles, preventing them from contracting. By targeting the neuromuscular junction, it inhibits the release of acetylcholine—a neurotransmitter that triggers muscle movement. This temporary “freeze” allows the skin above these muscles to appear smoother and more refreshed."
+                heading: "Hydration & Support",
+                description: "Hyaluronic acid or other active components bind with water molecules, plumping the skin and creating a firmer, more youthful look."
+            },
+            {
+                heading: "Collagen Boost",
+                description: "Some fillers stimulate the body’s natural collagen production, offering longer-lasting improvements in skin texture and elasticity."                
             }
         ],
         img: "/media/botox-what-and-how.png"
     },
     ourProcess: {
-        title: "Botox",
-        consultation: "Begin with a personalized consultation where our experts assess your skin and discuss your goals. We’ll create a tailored treatment plan to ensure optimal results that align with your needs.",
-        treatment: "During your Botox session, a skilled practitioner will administer precise injections to targeted areas.The procedure is quick, minimally invasive, and designed for your comfort.",
-        results: "Enjoy smoother, younger-looking skin as the Botox begins to work over the next few days. With minimal downtime, you’ll look refreshed and confident in no time."
+        title: "Dermal Fillers",
+        consultation: "Begin with a personalized consultation where our experts assess your facial structure, skin concerns, and aesthetic goals. We’ll create a tailored dermal filler treatment plan designed to deliver optimal results that align perfectly with your needs.",
+        treatment: "During your dermal filler session, a skilled practitioner will carefully administer precise injections to targeted areas. The procedure is relatively quick, minimally invasive, and designed for your comfort and safety.",
+        results: "Enjoy fuller, smoother, and more youthful-looking features as the filler integrates with your skin. While many notice immediate improvements, final results continue to refine over the next few days. With minimal downtime, you’ll look refreshed and confident in no time."
     },
     treatmentCards: {
         title: {
-            prefix: "Affordable Botox Treatments",
+            prefix: "Affordable Dermal Fillers Treatments",
             emphasis: "Tailored to You"
         },
-        description: "Affordable, transparent Botox pricing tailored to your goals and budget.",
-        regularUnit: "unit regular",
-        specialUnit: "unit",
+        description: "Affordable, transparent Dermal Fillers pricing tailored to your goals and budget.",
+        regularUnit: "syringe regular price",
+        specialUnit: "syringe new patient price",
         cards: [
             {
-                name: "Dysport",
-                regularPrice: 14,
-                specialPrice: 9.99,
+                name: "Restylane",
+                regularPrice: 799,
+                specialPrice: 599,
                 points: [
                     {
-                        title: "Targeted for Larger Areas",
-                        description: "Dysport’s unique diffusion pattern effectively addresses broader treatment zones."
+                        title: "Wide Range of Formulas",
+                        description: "With multiple Restylane variants, each designed for different areas and concerns, this family of fillers offers tailored solutions for lips, cheeks, under-eye hollows, and more."
                     },
                     {
-                        title: "Dynamic Wrinkle Reduction",
-                        description: "Designed to minimize the appearance of dynamic wrinkles across the face."
+                        title: "Precise Wrinkle Correction",
+                        description: "Smooths lines and folds, such as nasolabial folds, providing a naturally youthful appearance without looking overdone."
                     },
                     {
-                        title: "Rapid Action",
-                        description: "Spreads quickly to cover a wider surface for efficient, natural-looking results."
+                        title: "Minimal Downtime",
+                        description: "Most patients can resume daily activities almost immediately, making it a convenient option for busy lifestyles."
                     }
                 ]
             },
             {
-                name: "Botox",
-                regularPrice: 15,
-                specialPrice: 12.99,
+                name: "RHA Filler",
+                regularPrice: 799,
+                specialPrice: 599,
                 points: [
                     {
-                        title: "Versatile Treatment",
-                        description: "Botox targets facial muscles to address a variety of cosmetic concerns."
+                        title: "Adaptive Movement Technology",
+                        description: "RHA (Resilient Hyaluronic Acid) fillers are designed to move seamlessly with facial expressions, preserving a natural look even in highly dynamic areas of the face."
                     },
                     {
-                        title: "Wrinkle Reduction",
-                        description: "Effectively minimizes fine lines and wrinkles for a smoother appearance."
+                        title: "Dynamic Wrinkle Reduction",
+                        description: "Ideal for treating moderate to deep dynamic wrinkles, such as around the mouth and eyes, with results that remain soft and flexible."
                     },
                     {
-                        title: "Natural Results",
-                        description: "Delivers a refreshed, rejuvenated look while preserving natural expressions."
+                        title: "Preserves Facial Expressions",
+                        description: "Because of RHA’s unique formulation, you’ll enjoy a rejuvenated appearance without compromising your authentic range of expression."
                     }
                 ]
             }
@@ -154,13 +157,13 @@ const dermal_fillers = {
         ],
     wantToLearnMore: {
         title: {
-            emphasis: "Botox is right"
+            emphasis: "Dermal Fillers is right"
         }
     },
     preparingForAppointment: {
         titlePrefix: "Preparing for Your",
-        titleEmphasis: "Botox Appointment",
-        subheading: "Here are some tips to ensure a smooth Botox experience:",
+        titleEmphasis: "Dermal Filler Appointment",
+        subheading: "Here are some tips to help ensure a comfortable, successful experience:",
         points: [
             {
                 heading: "Avoid Blood Thinners",
@@ -191,56 +194,74 @@ const dermal_fillers = {
         title: {
             first: "Understanding ",
             emphasis: "the Risks",
-            last: " of Botox"
+            last: " of Dermal Fillers"
         },
-        description: "Although Botox is considered safe when administered by a qualified provider, it’s important to be aware of the potential risks. Here are some possible side effects and how we manage them at Cadmen Clinic.",
+        description: "Although dermal fillers are generally considered safe when administered by an experienced professional, it’s important to be aware of the potential risks. Here are some possible side effects and how we manage them at Cadmen Clinic:",
         cards: [
             {
                 id: 1,
                 title: "Temporary Redness, Swelling, or Bruising",
-                subtitle: "These are the most common side effects and typically resolve within a day or two. Our injectors use advanced techniques to minimize bruising, and we provide aftercare guidance to help you recover quickly.",
+                subtitle: "These are the most common side effects and typically subside within a few days. Our injectors use advanced techniques to minimize bruising, and we’ll provide detailed aftercare guidance (like cold compresses) to help you recover quickly.",
                 img: "/media/services/botox/redness.png"
             },
             {
                 id: 2,
-                title: "Headaches or Minor Discomfort",
-                subtitle: "Some clients experience mild headaches or discomfort after Botox. These effects are usually short-lived and can be managed with over-the-counter pain relief if needed.",
+                title: "Mild Discomfort or Tenderness",
+                subtitle: "Some clients experience minor soreness or pressure at the injection site. This usually fades within a few days and can often be managed with over-the-counter pain relief if needed.",
                 img: "/media/services/botox/headaches.png"
             },
             {
                 id: 3,
-                title: "Drooping Eyelid or Asymmetry",
-                subtitle: "Rarely, Botox can migrate to unintended muscles, potentially causing eyelid drooping or asymmetry. To minimize this risk, our injectors use precise techniques and provide post-treatment guidelines to prevent migration.",
+                title: "Lumps or Nodules",
+                subtitle: "Fillers can occasionally form small lumps or nodules under the skin. To reduce this risk, our skilled injectors use precise placement techniques and may gently massage the area immediately after injection. In some cases, lumps can be dissolved or adjusted with additional treatment.",
                 img: "/media/services/botox/eyelid.png"
             },
             {
                 id: 4,
-                title: "Allergic Reactions",
-                subtitle: "While very rare, allergic reactions to Botox can occur. During your consultation, we’ll review your medical history to ensure you’re a good candidate and discuss any potential allergies.",
+                title: "Asymmetry or Unintended Results",
+                subtitle: "If filler is not placed with precision, there’s a chance of unevenness or asymmetry. At Cadmen Clinic, our injectors have extensive training in facial anatomy to help ensure a balanced, natural-looking outcome. We also schedule follow-up appointments to make any necessary refinements.",
                 img: "/media/services/botox/allergy.png"
+            },
+            {
+                id: 5,
+                title: "Allergic Reactions",
+                subtitle: "While rare, allergic reactions to certain filler ingredients can occur. During your consultation, we’ll thoroughly review your medical history, discuss any known allergies, and help determine the safest product for you.",
+                img: "/media/services/dermal-fillers/dermalAllergicReactions.avif"
+            },
+            {
+                id: 6,
+                title: "Vascular Occlusion (VO)",
+                subtitle: "In very rare cases, filler can accidentally block a blood vessel, compromising blood flow to the surrounding tissue. This is a serious complication that requires prompt recognition and treatment. Our injectors are trained to identify early signs of VO, and we keep hyaluronidase on hand to dissolve the filler if needed. Quick action significantly reduces the risk of long-term effects.",
+                img: "/media/services/dermal-fillers/dermalVO.avif"
+            },
+            {
+                id: 7,
+                title: "Your Safety is Our Priority",
+                subtitle: "At Cadmen Clinic, we follow strict safety protocols, use FDA-approved fillers, and stay current with best practices to minimize risks. If you have any questions or concerns, our team is here to guide you through every step of the treatment process, ensuring a safe and positive experience.",
+                img: "/media/services/dermal-fillers/dermalYourSafety.avif"
             }
         ]
     },
     faqs: [
         {
             id: 1,
-            question: "How do I know if I’m a candidate?",
-            answer: "Come in for a complimentary consultation with our expert providers, who will assess your hair and scalp to recommend a customized treatment plan unique for you."
+            question: "How Soon Will I See Results?",
+            answer: "With most dermal fillers, you’ll notice instant volume and contour improvement right after your treatment. However, you may experience some swelling or mild bruising, which typically subsides within a few days. Final results often become apparent after 1–2 weeks once the filler fully settles."
         },
         {
             id: 2,
-            question: "When will I see results?",
-            answer: "From our experience with over 2,000 clients, results start to show within 3-4 sessions. It is critical to take good care of your scalp and hair outside of the clinic such as using safe shampoos and lotions."
+            question: "How Often Should I Get Dermal Fillers?",
+            answer: "The longevity of dermal fillers depends on the product used, the treatment area, and individual factors like your metabolism. Most fillers last anywhere from 6 to 18 months. Scheduling maintenance touch-ups as recommended by your injector helps you maintain consistent, natural-looking results over time."
         },
         {
             id: 3,
-            question: "Are there side effects or any recovery time needed?",
-            answer: "The great thing about PRP is that there is no recovery time needed - most patients feel good enough to immediately go back about their day."
+            question: "Can Fillers Help with Medical or Reconstructive Needs?",
+            answer: "While not as widely recognized for medical conditions as neuromodulators (like Botox), dermal fillers can sometimes be used for reconstructive purposes—for example, to correct certain facial asymmetries, fill acne scars, or address volume loss due to medical issues. Your provider can evaluate your specific needs and discuss if dermal fillers are an appropriate option."
         },
         {
             id: 4,
-            question: "Can I do PRP the same day of my consultation?",
-            answer: "Yes you can!"
+            question: "Are Dermal Fillers Only for Women?",
+            answer: "Absolutely not! Dermal fillers are a popular option for clients of all genders. At CADMEN Clinic, we tailor each filler treatment to your unique facial structure and desired outcome, regardless of gender. Many men opt for dermal fillers to enhance facial contours, soften lines, or add definition to the jawline—without appearing “overdone.”"
         }
     ]
 }
